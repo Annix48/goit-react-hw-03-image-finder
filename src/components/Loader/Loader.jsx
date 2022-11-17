f2c9f4a6eda7e85476be522cbe3bd5e2c6d08b6f
Anastasiia <101875515+Annix48@users.noreply.github.com>
2022-11-17 13:40:46 +0200

    done

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,32 +1,19 @@
+import { ColorRing } from 'react-loader-spinner';
 
-// import { ColorRing } from 'react-loader-spinner';
-// // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
-// <ColorRing
-//   visible={true}
-//   height="80"
-//   width="80"
-//   ariaLabel="blocks-loading"
-//   wrapperStyle={{}}
-//   wrapperClass="blocks-wrapper"
-//   colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-// />
-
-import { ColorRing } from 'react-loader-spinner'
-// import DotLoader from 'react-spinners/ClipLoader';
 import s from './Loader.module.css';
 
 export default function Loader() {
   return (
     <div className={s.overlay}>
-          <ColorRing
-      visible={true}
-  height="80"
-  width="80"
-  ariaLabel="blocks-loading"
-  wrapperStyle={{}}
-  wrapperClass="blocks-wrapper"
-  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}    />
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
     </div>
   );
 }
